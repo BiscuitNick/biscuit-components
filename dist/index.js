a21b959f04439a9cdcb98d59bf3ccd73d62d84c1
BiscuitNick <nick@biscuit.land>
2022-01-26 01:43:14 -0800

    0.5.2

--- dist/index.js
+++ dist/index.js
@@ -5,18 +5,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Badge", {
-  enumerable: true,
-  get: function get() {
-    return _Badge.default;
-  }
-});
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
-});
 Object.defineProperty(exports, "Dice", {
   enumerable: true,
   get: function get() {
@@ -29,10 +17,6 @@ Object.defineProperty(exports, "DiceClock", {
     return _DiceClock.DiceClock;
   }
 });
-
-var _Badge = _interopRequireDefault(require("./components/Badge"));
-
-var _Button = _interopRequireDefault(require("./components/Button"));
 
 var _Dice = _interopRequireDefault(require("./dice/Dice"));
 
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,4 @@
-import Badge from "./components/Badge";
-import Button from "./components/Button";
 import Dice from "./dice/Dice";
 import { DiceClock } from "./dice/DiceClock";
 
-export { Badge, Button, Dice, DiceClock };
+export { Dice, DiceClock };
